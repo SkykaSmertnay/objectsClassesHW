6dfd47ce57291c9f7257210adc72a04425dc9200
SkykaSmertnay <g925730@gmail.com>
2025-06-07 21:16:25 +0300

    Added all tasks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Author author1 = new Author("Лев", "Толстой" );
+        Author author2 = new Author("Николай","Карамзин");
+        Book book1 = new Book("Война и мир", author1,1865 );
+        System.out.println("\nКнига 1");
+        System.out.println(book1.getTitle());
+        System.out.println(book1.getAuthor().getName() + " " + book1.getAuthor().getSurname());
+        System.out.println(book1.getYear());
+        book1.setYear(1869);
+        System.out.println(book1.getYear());
+        System.out.println("\nКнига 2");
+        Book book2 = new Book("Бедная Лиза", author2, 1792);
+        System.out.println(book2.getTitle());
+        System.out.println(book2.getAuthor().getName() + " " + book2.getAuthor().getSurname());
+        System.out.println(book2.getYear());
+
+
     }
 }
